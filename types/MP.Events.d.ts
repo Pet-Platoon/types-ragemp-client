f3a24320c51a2460dda8133171fb9975fa20a0d9
Emre Kara <emre1702@mail.uni-paderborn.de>
2017-12-29 18:28:20 +0100

    Indent

--- types/MP.Events.d.ts
+++ types/MP.Events.d.ts
@@ -5,6 +5,6 @@ declare interface MpEvents {
     add(eventName: string, func: (...args: any[]) => void): void;
     call(eventName: string, ...args: any[]): void;
     addCommand(commandName: string, handlerFunction: (fullText: string, ...args: any[]) => void): void;
-	callRemote( eventName: string, ...args: any[] ): void;
-	remove( eventname: string, func: ( ...args: any[] ) => void ): void;
+    callRemote( eventName: string, ...args: any[] ): void;
+    remove( eventname: string, func: ( ...args: any[] ) => void ): void;
 }
